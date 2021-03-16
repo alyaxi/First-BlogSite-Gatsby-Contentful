2618aab9071d2f469ba7e070acf9ec7afbddda65
Syed Ali Uz Zaman <45460148+alyaxi@users.noreply.github.com>
2021-03-17 03:11:27 +0500

    Remove unused line

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -26,15 +26,7 @@ export const query  = graphql`
 
 
 const BlogPost = props => {
-    const options = {
-        renderNode: {
-          "embedded-asset-block": node => {
-            const alt = node.data.target.fields.title["en-US"]
-            const url = node.data.target.fields.file["en-US"].url
-            return <img alt={alt} src={url} />
-          },
-        },
-      }
+ 
     console.log(props);
     return (
         <Layout>
